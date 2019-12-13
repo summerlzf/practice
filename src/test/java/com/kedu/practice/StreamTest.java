d69db604326d143f1779b423ed92a447685df509
liuzifeng <liuzifeng@vivo.com>
2019-12-13 16:56:51 +0800

    流Stream的findFirst和map的使用测试

---
+++ src/test/java/com/kedu/practice/StreamTest.java
@@ -0,0 +1,26 @@
+package com.kedu.practice;
+
+import com.kedu.practice.entity.UserInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author liuzifeng
+ */
+@SpringBootTest
+public class StreamTest {
+
+    @Test
+    public void test() {
+        UserInfo vo = new UserInfo();
+        vo.setId(1);
+        vo.setName("");
+        List<UserInfo> list = new ArrayList<>();
+        list.add(vo);
+        String name = list.stream().findFirst().map(UserInfo::getName).orElse("[empty]");
+        System.out.println("name: " + name);
+    }
+}
