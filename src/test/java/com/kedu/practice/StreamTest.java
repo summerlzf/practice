bb49d3256c4081187f6a74e8d460f9683a854048
liuzifeng <liuzifeng@vivo.com>
2019-12-16 16:52:57 +0800

    stream

--- src/test/java/com/kedu/practice/StreamTest.java
+++ src/test/java/com/kedu/practice/StreamTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author liuzifeng
@@ -21,6 +22,11 @@ public class StreamTest {
         List<UserInfo> list = new ArrayList<>();
         list.add(vo);
         String name = list.stream().findFirst().map(UserInfo::getName).orElse("[empty]");
+        System.out.println("name: " + name);
+        if (name.length() == 0) {
+            vo = null;
+        }
+        name = Optional.ofNullable(vo).map(UserInfo::getName).orElse("{empty}");
         System.out.println("name: " + name);
     }
 }
